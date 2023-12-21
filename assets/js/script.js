4fe74eb892a4469d5ac40b8b8d018c1037214fbd
Shyam sundhar Kunduru <123252647+shyamsundharkunduru-brs@users.noreply.github.com>
2023-12-21 09:50:34 +0530

    updating apps

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ const canvas = document.getElementById("draw");
 canvas.width = canvas.clientWidth;
 canvas.height = canvas.clientHeight;
 
-const ctx = canvas.getContext("2d"); // 2 prams - context type and context attributes
+const ctx = canvas.getContext("2d");
 
 // Default theme
 let chathams_blue = "#1A4B84";
@@ -16,27 +16,21 @@ ctx.lineWidth = 10;
 
 // Init
 let isDrawing = false;
+let isErasing = false; // New variable for erasing
 let lastX = 0;
 let lastY = 0;
 let hue = 0;
 
 function draw(e) {
-  if (!isDrawing) return; // Check for mouse click
+  if (!isDrawing || isErasing) return;
+
   ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
 
-  ctx.beginPath(); //Begin a new path
-
-  // Start drawing the line
+  ctx.beginPath();
   ctx.moveTo(lastX, lastY);
-  console.log(`LAST X - ${lastX}`);
-  console.log(`LAST Y - ${lastY}`);
-
-  // Go to current mouse location
   ctx.lineTo(e.offsetX, e.offsetY);
-  console.log(`CURRENT X - ${e.offsetX}`);
-  console.log(`CURRENT Y - ${e.offsetY}`);
-
   ctx.stroke();
+
   [lastX, lastY] = [e.offsetX, e.offsetY];
 
   hue++;
@@ -45,15 +39,31 @@ function draw(e) {
   }
 }
 
+function erase(e) {
+  if (!isDrawing || !isErasing) return;
+
+  ctx.clearRect(e.offsetX - 5, e.offsetY - 5, 10, 10);
+}
+
 // Event Listeners
 canvas.addEventListener("mousedown", (e) => {
   isDrawing = true;
-  [lastX, lastY] = [e.offsetX, e.offsetY]; //Mouse cursor's coordinates
+  isErasing ? erase(e) : ([lastX, lastY] = [e.offsetX, e.offsetY]);
 });
 
-canvas.addEventListener("mousemove", draw);
+canvas.addEventListener("mousemove", (e) => {
+  draw(e);
+  erase(e); // Erase while moving if erasing mode is active
+});
+
 canvas.addEventListener("mouseup", () => (isDrawing = false));
 canvas.addEventListener("mouseout", () => (isDrawing = false));
+
+// Toggle between draw and erase modes
+document.getElementById("toggleMode").addEventListener("click", () => {
+  isErasing = !isErasing;
+  document.getElementById("toggleMode").innerText = isErasing ? "Draw Mode" : "Erase Mode";
+});
 
 // Set theme
 function setTheme(theme) {
